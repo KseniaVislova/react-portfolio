44057c7aafb9e3a48c88dba671dea7928aacc315
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-02-10 19:15:34 +0300

    feat: add styles and animations for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,28 @@
 import React from "react";
-import './Header.module.css';
+import styles from './Header.module.css';
+import classnames from "classnames";
 
 function Header() {
   return (
-    <div>
-        <div>
-          KV
+    <div className={styles.header}>
+        <div className={styles.logo}>
+          <div className={styles.block}>
+          <code>dev</code>
+          </div>
+          <div className={styles.block}>
+          </div>
+          <span>KV</span>
         </div>
-          <nav class="nav">
-            <ul>
-              <li><a href="#contacts">Contacts</a></li>
-              <li><a href="#summary">Summary</a></li>
-              <li><a href="#skills">Skills</a></li>
-              <li><a href="#code">Code</a></li>
-              <li><a href="#experience">Experience</a></li>
-              <li><a href="#projects">Projects</a></li>
-              <li><a href="#education">Education</a></li>
-              <li><a href="#language">Language</a></li>
+          <nav className={styles.nav}>
+            <ul className={styles.nav__list}>
+              <li><a href="#contacts" className={classnames([styles.nav__link, styles.active])}>Contacts</a></li>
+              <li><a href="#summary" className={styles.nav__link}>Summary</a></li>
+              <li><a href="#skills" className={styles.nav__link}>Skills</a></li>
+              <li><a href="#code" className={styles.nav__link}>Code</a></li>
+              <li><a href="#experience" className={styles.nav__link}>Experience</a></li>
+              <li><a href="#projects" className={styles.nav__link}>Projects</a></li>
+              <li><a href="#education" className={styles.nav__link}>Education</a></li>
+              <li><a href="#language" className={styles.nav__link}>Language</a></li>
             </ul>
         </nav>
     </div>
