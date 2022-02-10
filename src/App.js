e99fa992c70f3b37c37692e9967018a003406dc9
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-02-10 20:29:25 +0300

    add: feat: add routes for app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styles from './App.module.css';
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
@@ -11,18 +12,31 @@ import Projects from './components/Projects/Projects'
 import Footer from './components/Footer/Footer'
 
 function App() {
+  const pages = ['hero', 'skills', 'experience', 'languages', 'education', 'code', 'projects']
+  const navList = ['/skills', '/experience', '/languages', '/education', '/code', '/projects']
+
+  const [page, setPage] = useState('hero')
+
+  const changePage = (e) => {
+    e.target.text !== undefined ? setPage(e.target.text) : setPage('hero');
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <Header />
-      <Hero />
-      <Skills />
-      <Experience />
-      <Languages />
-      <Education />
-      <Code />
-      <Projects />
+    <BrowserRouter>
+      <div className={styles.wrapper}>
+      <Header changePage={changePage} pages={navList} page={page}/>
+      <Routes>
+        <Route path="/" exact element={<Hero />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/languages" element={<Languages />} />
+        <Route path="/education"element={<Education />} />
+        <Route path="/code" element={<Code />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
       <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from './Header.module.css';
 import classnames from "classnames";
 
-function Header() {
+function Header({changePage, pages, page}) {
+
   return (
     <div className={styles.header}>
-        <div className={styles.logo}>
+        <Link to="/" className={classnames([styles.logo], {[styles.logo_active]: page === 'hero'})} onClick={changePage} >
           <div className={styles.block}>
           <code>dev</code>
           </div>
           <div className={styles.block}>
           </div>
           <span>KV</span>
-        </div>
-          <nav className={styles.nav}>
-            <ul className={styles.nav__list}>
-              <li><a href="#contacts" className={classnames([styles.nav__link, styles.active])}>Contacts</a></li>
-              <li><a href="#summary" className={styles.nav__link}>Summary</a></li>
-              <li><a href="#skills" className={styles.nav__link}>Skills</a></li>
-              <li><a href="#code" className={styles.nav__link}>Code</a></li>
-              <li><a href="#experience" className={styles.nav__link}>Experience</a></li>
-              <li><a href="#projects" className={styles.nav__link}>Projects</a></li>
-              <li><a href="#education" className={styles.nav__link}>Education</a></li>
-              <li><a href="#language" className={styles.nav__link}>Language</a></li>
-            </ul>
+        </Link>
+          <nav className={styles.nav__list}>
+              {pages.map(item => (
+                <Link to={item}  onClick={changePage} className={classnames([styles.nav__link], {[styles.active]: page === item.slice(1)})}>{item.slice(1)}</Link>
+              ))}
         </nav>
     </div>
   );
